9f9c33823e1db013d2d0a40701b0af871a3d208c
junkichi <122D037@stu.nit.ac.jp>
2025-11-12 11:43:28 +0900

    STSの追加

--- drawer/src/com/objetdirect/gwt/umldrawer/client/beans/UmlRelation.java
+++ drawer/src/com/objetdirect/gwt/umldrawer/client/beans/UmlRelation.java
@@ -9,4 +9,12 @@ public class UmlRelation {
     public String sourceMultiplicity;
     public String targetMultiplicity;
     // 必要に応じてコンストラクタやメソッドを追加
+ // ▼▼▼ 追加したフィールド ▼▼▼
+    // Python側の line_style に対応
+    public String lineStyle;
+    // Python側の source_arrow に対応
+    public String sourceArrow;
+    // Python側の target_arrow に対応
+    public String targetArrow;
+    // ▲▲▲ 追加ここまで ▲▲▲
 }
